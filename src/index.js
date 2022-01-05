c91768911f967b2cd6003e2d7b0b4b77d9ba5f75
yarode <jaspervdvorst@gmail.com>
2022-01-05 14:12:15 +0800

    restructured index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { Client } = require('discord.js')
 const dotenv = require('dotenv')
-const { getSomeData } = require('./fetchData')
+const { getStaked, getVested, getClaimed } = require('./fetchData')
 
 const { numberWithCommas } = require('./utils')
 
@@ -8,26 +8,50 @@ dotenv.config()
 
 const client = new Client()
 
+let next = 0
+
 // eslint-disable-next-line
 client.on('ready', () => {
   console.log(`Bot successfully started as ${client.user.tag}`)
-  client.user.setActivity(
-    `current block number`,
-    { type: 'WATCHING' },
-  )
 })
+
+async function() updateBot(nickname, status) {
+  client.guilds.cache.forEach(async (guild) => {
+    const botMember = guild.me
+    await botMember.setNickname(numberWithCommas(nickname))
+  })
+
+  client.user.setActivity(status[0], status[1])
+
+  console.log(`Updated nickname to - ${nickname}`)
+  console.log(`Updated status to - Watching: ${status[0]}`)
+}
 
 // Updates treasury balance on bot's status every X amount of time
 client.setInterval(async () => {
+  if(!next) {
+    // display staked bMag
+    staked = await getStaked()
+    status = ['Staked bMAG', { type: 'WATCHING' }]
+    await updateBot(staked, staked)
 
-  const data = await getSomeData()
+    next = 1
+  } if(next == 1) {
+    // display vested bMag
+    vested = await getVested()
+    status = ['Vested bMAG', { type: 'WATCHING' }]
+    await updateBot(vested, status)
 
-  client.guilds.cache.forEach(async (guild) => {
-    const botMember = guild.me
-    await botMember.setNickname(`$${data}`)
-  })
+    next = 2
+  } else {
+    // display claimed bMag
+    claimed = await getClaimed()
+    status = ['Claimed bMAG', { type: 'WATCHING' }]
+    await updateBot(claimed, status)
+    
+    next = 0
+  }
 
-  console.log(`Updated to - $${data}`)
 }, parseInt(process.env.INTERVAL) * 60 * 1000)
 
 client.login(process.env.DISCORD_API_TOKEN)
